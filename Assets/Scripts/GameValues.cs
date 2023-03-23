54a78710deaca731a54802bac85c92667ee21289
jason <clarkjasonkong@gmail.com>
2023-03-23 15:51:57 +0800

    Cleaned up GameValues
    ~just a wee touchup before proceeding

--- Assets/Scripts/GameValues.cs
+++ Assets/Scripts/GameValues.cs
@@ -7,98 +7,41 @@ public class GameValues : ScriptableObject
 {
     #region Game Values
 
-    [SerializeField] [Range(0.1f, 20f)] private float _player_base_speed;
-    public float PlayerBaseSpeed
-    {
-        get { return _player_base_speed; }
-    }
+    [SerializeField] [Range(0.1f, 20f)] public float PlayerBaseSpeed;
 
     // USAGE: movespeed = _player_base_speed - (_player_level * _player_speed_decrease_multiplier)
-    [SerializeField] [Range(0.1f, 1.0f)] private float _player_speed_decrease_multiplier;
-    public float PlayerSpeedDecreaseMultiplier
-    {
-        get { return _player_speed_decrease_multiplier; }
-    }
+    [SerializeField] [Range(0.1f, 1.0f)] public float PlayerSpeedDecreaseMultiplier;
 
     // USAGE: replaces hole transform.localScale;
-    [SerializeField] [Range(0.1f, 1.5f)] private float _hole_base_size;
-    public float HoleBaseSize
-    {
-        get { return _hole_base_size; }
-    }
-
-    [SerializeField] [Range(0.1f, 10f)] private float _hole_grow_speed;
-    public float HoleGrowSpeed
-    {
-        get { return _hole_grow_speed; }
-    }
-
+    [SerializeField] [Range(0.1f, 1.5f)] public float HoleBaseSize;
+    
+    [SerializeField] [Range(0.1f, 10f)] public float HoleGrowSpeed;
+    
     // USAGE: size += _hole_level* _hole_base_size*_hole_size_multiplier
-    [SerializeField][Range(0.1f, 1.5f)] private float _hole_size_multiplier;
-    public float HoleSizeMultiplier
-    {
-        get { return _hole_size_multiplier; }
-    }
-    [SerializeField] [Range(0.1f, 2f)] private float _hole_absorb_strength;
-    public float HoleAbsorbStrength
-    {
-        get { return _hole_absorb_strength; }
-    }
+    [SerializeField][Range(0.1f, 1.5f)] public float HoleSizeMultiplier;
 
-    [SerializeField] [Range(1, 100)] private int _hole_base_exp_threshold;
-    public int HoleExpThreshold
-    {
-        get { return _hole_base_exp_threshold; }
-    }
+    [SerializeField] [Range(0.1f, 2f)] public float HoleAbsorbStrength;
+
+    [SerializeField] [Range(1, 100)] public int HoleExpThreshold;
+
     //USAGE: _hole_base_exp_threshold  + (_hole_base_exp_threshold *(_hole_level -1)* _hole_exp_threshold_multiplier)
-    [SerializeField][Range(0.5f, 1.5f)] private float _hole_exp_threshold_multiplier;
-    public float HoleExpThresholdMultiplier
-    {
-        get { return _hole_exp_threshold_multiplier; }
-    }
+    [SerializeField][Range(0.5f, 1.5f)] public float HoleExpThresholdMultiplier;
 
-    [SerializeField] [Range(1, 5)] private int _hole_absorb_difference;
-    public int HoleAbsorbDifference
-    {
-        get { return _hole_absorb_difference; }
-    }
+    [SerializeField] [Range(1, 5)] public int HoleAbsorbDifference;
 
     // the multiplier to be multiplied to the exp points from a hole absorbed by a bigger hole
-    [SerializeField] [Range(0.1f, 1.5f)] private float _hole_cannibal_exp_multiplier;
-    public float HoleCannibalExpMultiplier
-    {
-        get { return _hole_cannibal_exp_multiplier; }
-    }
+    [SerializeField] [Range(0.1f, 1.5f)] public float HoleCannibalExpMultiplier;
 
-    [SerializeField] [Range(0.1f, 2f)] private float _props_spawn_rate;
-    public float PropSpawnRate
-    {
-        get { return _props_spawn_rate; }
-    }
+    [SerializeField] [Range(0.1f, 2f)] public float PropSpawnRate;
 
     // USAGE: replaces prop transform.localScale;
-    [SerializeField] [Range(0.1f, 2f)] private float _props_base_size;
-    public float PropsBaseSize
-    {
-        get { return _props_base_size; }
-    }
+    [SerializeField] [Range(0.1f, 2f)] public float PropsBaseSize;
 
     // USAGE: size += _prop_size* _props_base_size*_props_size_multiplier
-    [SerializeField] [Range(0.1f, 1.5f)] private float _props_size_multiplier;
-    public float PropsSizeMultiplier
-    {
-        get { return _props_size_multiplier; }
-    }
+    [SerializeField] [Range(0.1f, 1.5f)] public float PropsSizeMultiplier;
 
-    [SerializeField] [Range(0f, 5f)] private float _camera_zoom_amount;
-    public float CameraZoomAmount
-    {
-        get { return _camera_zoom_amount; }
-    }
-    [SerializeField] [Range(0f, 10f)] private float _camera_zoom_speed;
-    public float CameraZoomSpeed
-    {
-        get { return _camera_zoom_speed; }
-    }
+    [SerializeField] [Range(0f, 5f)] public float CameraZoomAmount;
+    [SerializeField] [Range(0f, 10f)] public float CameraZoomSpeed;
+
     #endregion
 }
