195e56cdc7fa9d14c2790954c9cc93cf855ae5c6
jason <clarkjasonkong@gmail.com>
2023-03-25 03:39:23 +0800

    Whirlpool Art Assest
    + added whirlpool sprites
    + spin
    +spin
    +sp+in

--- Assets/Scripts/SceneBootup.cs
+++ Assets/Scripts/SceneBootup.cs
@@ -12,9 +12,8 @@ public class SceneBootup : MonoBehaviour, IBootstrapper
     }
     public void LoadSingletonsAndDependencies()
     {
-        //GameManager.Instance.GameValues = ScriptableObjectsHelper.GetScriptableObject<GameValues>(FileNames.GAME_VALUES);
-
-        //GameManager.Instance.VisualValues = ScriptableObjectsHelper.GetScriptableObject<VisualValues>(FileNames.VISUAL_VALUES);
+        GameManager.Instance.GameValues = _game_values == null ? ScriptableObjectsHelper.GetScriptableObject<GameValues>(FileNames.GAME_VALUES) : _game_values;
+        GameManager.Instance.VisualValues = _visual_values == null ? ScriptableObjectsHelper.GetScriptableObject<VisualValues>(FileNames.VISUAL_VALUES) : _visual_values;
 
         GameManager.Instance.GameValues = _game_values;
         GameManager.Instance.VisualValues = _visual_values;
